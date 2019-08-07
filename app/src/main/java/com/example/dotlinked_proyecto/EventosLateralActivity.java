047b9bb0671e99645902562f7fc61cfdbf15b5f5
CASPC01 <josegonfer1977@gmail.com>
2019-08-07 15:20:27 +0200

    Añadida dependencia androidx.legacy:legacy-support-v4:1.0.0 compatible con implementation 'com.android.support:support-v4:27.0.2'

---
+++ app/src/main/java/com/example/dotlinked_proyecto/EventosLateralActivity.java
@@ -0,0 +1,14 @@
+package com.example.dotlinked_proyecto;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EventosLateralActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_eventos_lateral);
+    }
+}
